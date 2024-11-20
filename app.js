3ced905ea735d6a375edb84ef283643d3a38c1dc
kyy08 <kyyelzz1@gmail.com>
2024-11-20 15:34:12 +0700

    middleware

--- app.js
+++ app.js
@@ -8,26 +8,47 @@ const expressLayout = require('express-ejs-layouts')
 
 const db = require('./database/db.js')
 
+// pertemuan 7 session dan bycrypt
+const session = require('express-session');
+const authRoutes = require('./routes/authRoutes');
+const { isAuthenticated } = require('./middlewares/middleware.js');
+
+
+app.use(express.static('public'));
 app.use(expressLayout);
 
 app.use(express.json());
 
+
+// Konfigurasi express-session
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false }  //Set ke true jika menggunakan HTTPS
+}));
+
+
 app.use('/todos', todoRoutes);
 app.set('view engine', 'ejs');
-app.get('/', (req, res) => {
+app.get('/',isAuthenticated, (req, res) => {
     res.render('index', {
         layout: 'layouts/main-layouts.ejs'
     });
 });
 
-app.get('/contact', (req, res) => {
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', authRoutes);
+
+app.get('/contact',isAuthenticated,(req, res) => {
     res.render('contact', {
         layout: 'layouts/main-layouts.ejs'
     });
         
 });
 
-app.get('/todo-view', (req, res) => {
+app.get('/todo-view',isAuthenticated, (req, res) => {
     db.query('SELECT * FROM todos', (err, todos) => {
         if (err) return res.status(500).send('Internal Server Error');
         res.render('todo', {
---
+++ dependency.mjs
@@ -0,0 +1,6 @@
+import _ from "lodash";
+
+const source = "Mohamad Wahyu R A"
+const target = _.capitalize(source)
+
+console.info(target);
